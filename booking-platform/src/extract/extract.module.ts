0bf52767e1831a3b8227e3ec1a570f7f63b87430
bricematias <83917860+bricematias@users.noreply.github.com>
2025-05-19 11:39:43 +0200

    add grpc

--- booking-platform/src/extract/extract.module.ts
+++ booking-platform/src/extract/extract.module.ts
@@ -4,11 +4,12 @@ import { Reservation } from '../reservation/reservation.entity';
 import { ExtractsService } from './extract.service';
 import { ExtractsController } from './extract.controller';
 import { MinioModule } from '../minio/minio.module';
+import {ExtractsGrpcService} from "./extracts.grpc.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Reservation]), MinioModule],
     controllers: [ExtractsController],
-    providers: [ExtractsService],
+    providers: [ExtractsService,ExtractsGrpcService],
     exports: [ExtractsService],
 })
 export class ExtractsModule {}
